a9b81c2726269fdc8e91d417a4dd3e7f9ae0c868
Maxime Ballarotta <maxballarotta@gmail.com>
2020-09-21 08:56:20 +0200

    Results are now saved in the result folder +  Add example_plot_results

--- src/mod_plot.py
+++ src/mod_plot.py
@@ -4,7 +4,8 @@ import logging
 import matplotlib.pylab as plt
 from matplotlib.ticker import ScalarFormatter
 
-def plot_psd_score(ds_psd):
+
+def plot_psd_score_v0(ds_psd):
         
     try:
         nb_experiment = len(ds_psd.experiment)
@@ -139,3 +140,49 @@ def hvplot_demo_obs_karin(list_of_dataset, dc_ref, central_date, delta_t):
                                              height=300, width=400, cmap = 'gist_stern', datashade=True)
 
     return plot
+
+
+def plot_psd_score(ds):
+    
+    plt.figure(figsize=(8, 6))
+    ax = plt.gca()
+    #ax.invert_yaxis()
+    #ax.invert_xaxis()
+    c1 = plt.contourf(1./(ds['freq_lon']), 1./ds['freq_time'], ds['psd_score'],
+                      levels=numpy.arange(0,1.1, 0.1), cmap='RdYlGn', extend='both')
+    cbar = plt.colorbar(pad=0.01)
+    plt.xlabel('wavelenght(degree_lon)', fontweight='bold', fontsize=20)
+    plt.ylabel('wavelenght (days)', fontweight='bold', fontsize=20)
+    #plt.xscale('log')
+    #plt.yscale('log')
+    plt.grid(linestyle='--', lw=1, color='w')
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.title('PSD-based score', fontweight='bold', fontsize=20)
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_major_formatter(ScalarFormatter())
+    c2 = plt.contour(1./(ds['freq_lon']), 1./ds['freq_time'], ds['psd_score'], levels=[0.5], linewidths=2, colors='k')
+    cbar.add_lines(c2)
+
+    bbox_props = dict(boxstyle="round,pad=0.5", fc="w", ec="k", lw=2)
+    ax.annotate('Resolved scales',
+            xy=(1.15, 0.8),
+            xycoords='axes fraction',
+            xytext=(1.15, 0.55),
+            bbox=bbox_props,
+            arrowprops=
+                dict(facecolor='black', shrink=0.05),
+                horizontalalignment='left',
+                verticalalignment='center')
+
+    ax.annotate('UN-resolved scales',
+            xy=(1.15, 0.2),
+            xycoords='axes fraction',
+            xytext=(1.15, 0.45),
+            bbox=bbox_props,
+            arrowprops=
+                dict(facecolor='black', shrink=0.05),
+                horizontalalignment='left',
+                verticalalignment='center')
+    
+    plt.show()
