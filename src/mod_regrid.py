d8ae3f7bbcd2237d6fe0d55a31d8fd564eb27d76
Maxime Ballarotta <maxballarotta@gmail.com>
2022-08-12 09:29:43 +0200

    Update mod_regrid.py

--- src/mod_regrid.py
+++ src/mod_regrid.py
@@ -1,6 +1,7 @@
 import xarray as xr
 import numpy
 import pyinterp
+import pyinterp.fill
 import logging 
 
 def oi_regrid(ds_source, ds_target):
@@ -24,10 +25,21 @@ def oi_regrid(ds_source, ds_target):
                                      mx_target.flatten(),
                                      my_target.flatten(),
                                      mz_target.flatten(),
-                                     bounds_error=True).reshape(mx_target.shape).T
+                                     bounds_error=False).reshape(mx_target.shape).T
+    
+    # MB add extrapolation in NaN values if needed
+    if numpy.isnan(ssh_interp).any():
+        logging.info('     NaN found in ssh_interp, starting extrapolation...')
+        x_source_axis = pyinterp.Axis(ds_target['lon'].values, is_circle=False)
+        y_source_axis = pyinterp.Axis(ds_target['lat'].values)
+        z_source_axis = pyinterp.TemporalAxis(ds_target["time"][:].values)
+        grid = pyinterp.Grid3D(x_source_axis, y_source_axis, z_source_axis,  ssh_interp.T)
+        has_converged, filled = pyinterp.fill.gauss_seidel(grid)
+    else:
+        filled = ssh_interp.T
     
     # Save to dataset
-    ds_ssh_interp = xr.Dataset({'sossheig' : (('time', 'lat', 'lon'), ssh_interp)},
+    ds_ssh_interp = xr.Dataset({'sossheig' : (('time', 'lat', 'lon'), filled.T)},
                                coords={'time': ds_target['time'].values,
                                        'lon': ds_target['lon'].values, 
                                        'lat': ds_target['lat'].values, 
